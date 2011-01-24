b4ffb64ae0bb133202e54977322e22aea534b722
Kevin Downey (hiredman) <redchin@gmail.com>
2011-01-23 17:36:56 -0800

    dep on clojure is implied, and clj-growl doesn't use contrib at all

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject Howler "1.0.0-SNAPSHOT"
   :description "irssi rawlog to SQS"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [fnparse "2.2.7"]
                  [com.google.code.typica/typica "1.7.2"]
-                 [clj-growl "0.2.1" :exclusions [org.clojure/clojure-contrib]]]
+                 [clj-growl "0.2.1" :exclusions [org.clojure/clojure-contrib
+                                                 org.clojure/clojure]]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main Howler.core)
