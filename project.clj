64ed9452ee6681c6d974b06d876f2c409f4f1aae
Kevin Downey (hiredman) <redchin@gmail.com>
2011-06-07 12:10:30 -0700

    new clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject Howler "1.0.0-SNAPSHOT"
   :description "irssi rawlog to SQS"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [fnparse "2.2.7"]
                  [com.google.code.typica/typica "1.7.2"]
                  [clj-growl "0.2.1" :exclusions [org.clojure/clojure-contrib
