88ad13b85ae9bb78bf5c8a6dda2458846079c294
Kevin Downey <redchin@gmail.com>
2011-09-09 23:10:35 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "irssi rawlog to SQS"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [fnparse "2.2.7"]
+                 [clj-http "0.1.1"]
+                 [commons-codec "1.4"]
                  [com.google.code.typica/typica "1.7.2"]
                  [clj-growl "0.2.1" :exclusions [org.clojure/clojure-contrib
-                                                 org.clojure/clojure]]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+                                                 org.clojure/clojure]]
+                 [storage "1.0.0-SNAPSHOT"]
+                 [org.clojure/tools.logging "0.1.2"]
+                 [ch.qos.logback/logback-classic "0.9.24"]]
   :main Howler.core)
